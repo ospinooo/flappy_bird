9e07023b9fd6e92309073897b8e63959e31b80bb
Ospino <ospino.p@gmail.com>
2019-09-06 18:54:18 +0200

    After 20 pipes we consider the nn has learned correctly, and we end the simulation
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- main.py
+++ main.py
@@ -140,8 +140,8 @@ def main(genomes, config):
                 nets.pop(x)
                 ge.pop(x)
 
-        if score > 50:
-            break
+        if score > 20:
+            run = False
 
         base.move()
         draw_window(win, birds, pipes, base, score, GEN)
